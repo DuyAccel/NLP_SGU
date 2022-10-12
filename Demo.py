848f95c23afd8da2e3be30a5f225dbf8406b8573
DuyAccel <hieuduyno1@gmail.com>
2022-10-12 10:42:21 +0700

    first push

---
+++ Demo.py
@@ -0,0 +1,17 @@
+from lib2to3.pgen2 import grammar
+import nltk
+# grammar1 = nltk.CFG.fromstring("""
+#   S -> NP VP
+#   VP -> V NP | V NP PP
+#   PP -> P NP
+#   V -> "saw" | "ate" | "walked"
+#   NP -> "John" | "Mary" | "Bob" | Det N | Det N PP
+#   Det -> "a" | "an" | "the" | "my"
+#   N -> "man" | "dog" | "cat" | "telescope" | "park"
+#   P -> "in" | "on" | "by" | "with"
+#   """)
+# sent = "Mary saw Bob".split()
+# rd_parser = nltk.RecursiveDescentParser(grammar1)
+# for tree in rd_parser.parse(sent):
+#     print(tree)
+nltk.app.rdparser()
